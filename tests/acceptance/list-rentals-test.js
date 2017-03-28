4ca659879724ec13521224b55189968586e1ce6b
Vinchenso <chensok@gmail.com>
2017-03-28 12:15:28 +0200

    add model and listing acceptance test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,16 +1,16 @@
-import { test } from 'qunit';
+import { test, skip } from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('should show rentals as the home page', function(assert) {
+test('should show rentals as the home page', (assert) => {
   visit('/');
   andThen(() => {
     assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 });
 
-test('should link to information about the company', function(assert) {
+test('should link to information about the company', (assert) => {
   visit('/');
   click('a:contains("About")');
   andThen(() => {
@@ -18,7 +18,7 @@ test('should link to information about the company', function(assert) {
   });
 });
 
-test('should link to contact information', function(assert) {
+test('should link to contact information', (assert) => {
  visit('/');
   click('a:contains("Contact")');
   andThen(() => {
@@ -26,16 +26,19 @@ test('should link to contact information', function(assert) {
   });
 });
 
-test('should list avalible rentals', function(assert) {
+test('should list avalible rentals', (assert) => {
+  visit('/');
+  andThen( () => {
+  assert.equal(find('.listing').length, 3, 'should equal 3 listings');
+  });
+});
+
+skip('should filter the list of rentals by city', (assert) => {
 
 });
 
-test('should filter the list of rentals by city', function(assert) {
 
-});
-
-
-test('should show details for a selected rental', function(assert) {
+skip('should show details for a selected rental', (assert) => {
 
 });
 
