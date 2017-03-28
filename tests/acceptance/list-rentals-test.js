e648042b1a16e4aa30472a2a43ccd7b8f66a3ac8
Vinchenso <chensok@gmail.com>
2017-03-28 11:34:57 +0200

    add NavBar and about,contact,home tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,14 +4,26 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should show rentals as the home page', function(assert) {
+  visit('/');
+  andThen(() => {
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
+});
 
 test('should link to information about the company', function(assert) {
-
+  visit('/');
+  click('a:contains("About")');
+  andThen(() => {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
 });
 
 test('should link to contact information', function(assert) {
-
+ visit('/');
+  click('a:contains("Contact")');
+  andThen(() => {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
 });
 
 test('should list avalible rentals', function(assert) {
